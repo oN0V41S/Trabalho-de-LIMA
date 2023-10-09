1633b78dc8e01c79e7b46684452edb80e4c93f4f
on0v41s <129956152+oN0V41S@users.noreply.github.com>
2023-10-09 14:00:55 +0000

    up

---
+++ src/components/carossel.js
@@ -0,0 +1,17 @@
+export default function Carousel() {
+  const images = [
+    {
+      src: "/images/image1.jpg",
+      alt: "Imagem 1",
+    },
+    {
+      src: "/images/image2.jpg",
+      alt: "Imagem 2",
+    },
+    {
+      src: "/images/image3.jpg",
+      alt: "Imagem 3",
+    },
+  ];
+  return <div></div>;
+}
---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import Carousel from "../components/carossel";
+
+export default function Home() {
+  return (
+    <body>
+      <Carousel />
+      <main className="´p-14">
+        <div className="p-4">Cabeçu de lampado</div>
+      </main>
+    </body>
+  );
+}
