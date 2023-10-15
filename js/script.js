06b8ed9d2705104e0c52f75ef841be42c2413d49
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2023-10-14 23:16:16 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//Array de imagens
+
+let Imagens = () => {
+    fetch("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json")
+      .then(retorno => retorno.json())
+      .then(dados => {
+        
+        const imagens = [];
+        const container = document.getElementById('mais-vistos');
+  
+        for (let i = 0; i < dados.items.length; i++) {
+          imagens.push(dados.items[i].image);
+        }
+  
+        let imagensHTML = 0;
+  
+        for(let i = 0; i < imagens.length; i++){
+          imagensHTML += `<img src="${imagens[i]}" />`;
+        }
+  
+        container.innerHTML = imagensHTML;
+  
+      });
+  }
+  
+  Imagens();
