70a7abe7dcbfb9f7cc74457c67ebb9776a0aec1f
oN0V41S <129956152+oN0V41S@users.noreply.github.com>
2023-10-16 09:36:37 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ let Imagens = () => {
           imagens.push(dados.items[i].image);
         }
   
-        let imagensHTML;
+        let imagensHTML = imagensHTML += `<img src="${imagens[0]}" />`;
 
         for(let i = 1; i < imagens.length; i++){
           imagensHTML += `<img src="${imagens[i]}" />`;
